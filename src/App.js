3bd02de3c9761de31f7de8ca39c2163e74ad4ef7
Sunny Bhandal <82823609+sunnybhandal@users.noreply.github.com>
2022-04-07 09:22:26 -0600

    files

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
 import './App.css';
 import { Navbar } from './components/Navbar/Navbar';
+import { About } from './components/About/About';
+import { Projects } from './components/Projects/Projects';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
-
+      <About />
+      <Projects />
     </div>
   );
 }
